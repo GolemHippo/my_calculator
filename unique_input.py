077bfaa0c817ae93641215c1f9d7815d4ab42540
Hippo <hippolyte.geslain@laplateforme.io>
2025-01-16 15:26:04 +0100

    fixed division by zero with try/except

--- unique_input.py
+++ unique_input.py
@@ -16,11 +16,7 @@ def multiply(x, y):
     return x * y
 
 def divide(x, y):
-    try:
-        return x / y
-    except ZeroDivisionError:
-        print("Division by zero is not allowed")
-        return None
+    return x / y
 
 def modulo(x,y):
     return x % y
@@ -42,6 +38,25 @@ def check_error():
             return False
         else:
             return True
+
+def check_parentheses(operators, numbers):
+    while '(' in operators:
+        #find the index of the first parenthese
+        p_index = operators.index('(')
+        #find the index of the closing parenthese
+        p_index2 = operators.index(')')
+        #isolate the operation between the parentheses
+        parenthese_operators = operators[p_index+1:p_index2]
+        #isolate the numbers between the parentheses
+        parenthese_numbers = numbers[p_index:p_index2]
+        #check for multiply or divide operators
+        multiply_divide(parenthese_numbers,parenthese_operators)
+        add_substract(parenthese_numbers,parenthese_operators)
+        #delete the parentheses from the lists
+        del operators[p_index:p_index2+1]
+        del numbers[p_index:p_index2]
+        #replace the parentheses by the result
+        numbers.insert(p_index,parenthese_numbers[0])
 
 #advance the calculus
 def update_calculus(operation,index,l_numbers,l_operators):
@@ -134,35 +149,25 @@ def menu(operators, numbers):
     while True:
         #ask the user for the operation
         og_input=input('Enter the operation with a space between each number and operator : x + y - z \nIf you want to exit calculation, enter "exit"\n:')
+        #exit the loop if the user wants to
         if og_input == 'exit':
             break
+        #sort the input
         sort_input(og_input)
+        #check for errors before starting the calculus
         if check_error() == True:
-            #check for parentheses
-            while '(' in operators:
-                #find the index of the first parenthese
-                p_index = operators.index('(')
-                #find the index of the closing parenthese
-                p_index2 = operators.index(')')
-                #isolate the operation between the parentheses
-                parenthese_operators = operators[p_index+1:p_index2]
-                #isolate the numbers between the parentheses
-                parenthese_numbers = numbers[p_index:p_index2]
-                #check for multiply or divide operators
-                multiply_divide(parenthese_numbers,parenthese_operators)
-                add_substract(parenthese_numbers,parenthese_operators)
-                #delete the parentheses from the lists
-                del operators[p_index:p_index2+1]
-                del numbers[p_index:p_index2]
-                #replace the parentheses by the result
-                numbers.insert(p_index,parenthese_numbers[0])
-            #finish the calculus
-            multiply_divide(numbers,operators)
-            add_substract(numbers,operators)
-            #print the result
-            print(og_input,'=',numbers[0])
-            #save the operation in the history
-            history.append(og_input + ' = ' + str(numbers[0]))
+            try:
+                #check for parentheses
+                check_parentheses(operators, numbers)
+                #finish the calculus
+                multiply_divide(numbers,operators)
+                add_substract(numbers,operators)
+                #print the result
+                print(og_input,'=',numbers[0])
+                #save the operation in the history
+                history.append(og_input + ' = ' + str(numbers[0]))
+            except ZeroDivisionError:
+                print('Error, division by zero')
         else:
             print('Error, please try again')
         #clear memory
