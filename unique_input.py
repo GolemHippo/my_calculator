679bb5fe95600376b85275da8b73b20add515763
Hippo <hippolyte.geslain@laplateforme.io>
2025-01-16 10:49:50 +0100

    refacto multiple functions, added parentheses

--- unique_input.py
+++ unique_input.py
@@ -1,6 +1,8 @@
 #initate the numbers and operators lists
 numbers = []
 operators = []
+l_numbers = []
+l_operators = []
 
 #define the operations functions
 def add(x, y):
@@ -17,6 +19,7 @@ def divide(x, y):
 
 def modulo(x,y):
     return x % y
+
 
 #define the sorting function for operators and numbers
 def sort_input(og_input):
@@ -36,52 +39,78 @@ def check_error():
             break
 
 #advance the calculus
-def update_calculus(parameter,index):
-    #save the result in a variable
-                result = parameter(numbers[index],numbers[index+1])
+def update_calculus(operation,index,l_numbers,l_operators):
+                #save the result in a variable
+                result = operation(l_numbers[index],l_numbers[index+1])
                 #replace the first number by the result
-                numbers[index] = result
+                l_numbers[index] = result
                 #remove the operator from the list
-                operators.pop(index)
+                l_operators.pop(index)
                 #remove the second number from the list
-                numbers.pop(index+1)
+                l_numbers.pop(index+1)
 
-#input menu
-def menu():
-    while True:
-        #ask the user for the operation
-        og_input=input('Enter the operation like this : x + y - z \n:')
-        sort_input(og_input)
-        check_error()
-        #check for multiply or divide operators
-        while '*' in operators or '/' in operators:
-            #isolate and execute each operator in the list
-            for index, operator in enumerate(operators):
+#check for multiply or divide operators
+def multiply_divide(l_numbers,l_operators):
+     while '*' in l_operators or '/' in l_operators:
+            #isolate and execute each operation in the list
+            for index, operator in enumerate(l_operators):
                 if operator == '*':
                     #set the parameters for the function
-                    parameter = multiply
+                    operation = multiply
                     #call the function
-                    update_calculus(parameter,index)
+                    update_calculus(operation,index,l_numbers,l_operators)
                     break
                 if operator == '/':
-                    parameter = divide
-                    update_calculus(parameter,index)
+                    operation = divide
+                    update_calculus(operation,index,l_numbers,l_operators)
                     break
-        #check for addition or substract operators
-        while '+' in operators or '-' in operators:
-            for index, operator in enumerate(operators):
-                if operator == '+':
-                    parameter = add
-                    update_calculus(parameter,index)
-                    break
-                if operator == '-':
-                    parameter = substract
-                    update_calculus(parameter,index)
-                    break
+
+#check for addition or substract operators
+def add_substract(l_numbers,l_operators):
+    while '+' in l_operators or '-' in l_operators:
+        for index, operator in enumerate(l_operators):
+            if operator == '+':
+                operation = add
+                update_calculus(operation,index,l_numbers,l_operators)
+                break
+            if operator == '-':
+                operation = substract
+                update_calculus(operation,index,l_numbers,l_operators)
+                break
+
+#menu function
+def menu(operators, numbers):
+    while True:
+        #ask the user for the operation
+        og_input=input('Enter the operation with a space between each number and operator : x + y - z \n:')
+        sort_input(og_input)
+        check_error()
+        #check for parentheses
+        while '(' in operators:
+            #find the index of the first parenthese
+            p_index = operators.index('(')
+            #find the index of the closing parenthese
+            p_index2 = operators.index(')')
+            #isolate the operation between the parentheses
+            parenthese_operators = operators[p_index+1:p_index2]
+            #isolate the numbers between the parentheses
+            parenthese_numbers = numbers[p_index:p_index2]
+            #check for multiply or divide operators
+            multiply_divide(parenthese_numbers,parenthese_operators)
+            add_substract(parenthese_numbers,parenthese_operators)
+            #delete the parentheses from the lists
+            del operators[p_index:p_index2+1]
+            del numbers[p_index:p_index2]
+            #replace the parentheses by the result
+            numbers.insert(p_index,parenthese_numbers[0])
+        #finish the calculus
+        multiply_divide(numbers,operators)
+        add_substract(numbers,operators)
+        #print the result
         print(og_input,'=',numbers[0])
         #clear memory
         numbers.clear()
         operators.clear()
 
 if __name__ == '__main__':
-    menu()
+    menu(operators, numbers)
